0e70f96397e7c8f8d05f39b4690206bf343b9172
Yuri.Carvalho <yuri9895@gmail.com>
2022-09-28 06:12:42 -0300

    otimizações

--- js/script.js
+++ js/script.js
@@ -1,76 +1,35 @@
-// Ativar Links do Menu
+const params = new URLSearchParams(location.search);
+
+params.forEach((item) => {
+  const element = document.querySelector(`[type="radio"][value="${item}"]`);
+  if (element) element.checked = true;
+});
+
+const dts = document.querySelectorAll("dt");
+
+dts.forEach((dt) => {
+  dt.addEventListener("click", () => {
+    dt.parentElement.classList.toggle("ativa");
+  });
+});
+
 const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link) {
-  const url = location.href;
-  const href = link.href;
-
-  url.includes;
-
-  if (url.includes(href)) {
+links.forEach((link) => {
+  if (location.href.includes(link.href)) {
     link.classList.add("ativo");
   }
-}
-links.forEach(ativarLink);
-
-// Ativar Itens do Orçamento
-
-const parametros = new URLSearchParams(location.search);
-
-function ativarProduto(parametro) {
-  const elemento = document.getElementById(parametro);
-  if (elemento) {
-    elemento.checked = true;
-  }
-
-  console.log(elemento);
-}
-
-parametros.forEach(ativarProduto);
-
-// Perguntas Frequentes
-
-const perguntas = document.querySelectorAll(".perguntas button");
-
-function ativarPergunta(event) {
-  const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute("aria-controls");
-  const resposta = document.getElementById(controls);
-
-  resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains("ativa");
-  console.log(ativa);
-  pergunta.setAttribute("aria-expanded", ativa);
-}
-
-function eventosPerguntas(pergunta) {
-  pergunta.addEventListener("click", ativarPergunta);
-}
-
-perguntas.forEach(eventosPerguntas);
-
-console.log(perguntas);
-
-// Galeria de bicicletas
+});
 
 const galeria = document.querySelectorAll(".bicicleta-imagens img");
 const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
-function trocarImagem(event) {
-  const img = event.currentTarget;
-  matchMedia("(min-width:  1000px)").matches;
-  if (media) {
-    galeriaContainer.prepend(img);
-  }
-}
+galeria.forEach((img) => {
+  img.addEventListener("click", () => {
+    if (matchMedia("(min-width: 1000px)").matches) {
+      galeriaContainer.prepend(img);
+    }
+  });
+});
 
-function eventosGaleria(img) {
-  img.addEventListener("click", trocarImagem);
-}
-
-galeria.forEach(eventosGaleria);
-
-// Animação
-if (window.SimpleAnime) {
-  new SimpleAnime();
-}
+// if (window.SimpleAnime) new SimpleAnime();
